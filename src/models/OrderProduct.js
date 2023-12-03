b201b91ffa4b28ec46348fc519262afd381a65c8
Thau Nguyen <nghoangthau@gmail.com>
2023-12-03 18:09:19 +0700

    updat product model

--- src/models/OrderProduct.js
+++ src/models/OrderProduct.js
@@ -1,25 +1,26 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-const orderSchema = new mongoose.Schema({
+const orderSchema = new mongoose.Schema(
+  {
     orderItems: [
-        {
-            name: { type: String, required: true },
-            amount: { type: Number, required: true },
-            image: { type: String, required: true },
-            price: { type: Number, required: true },
-            discount: { type: Number },
-            product: {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: 'Product',
-                required: true,
-            },
+      {
+        name: { type: String, required: true },
+        amount: { type: Number, required: true },
+        image: { type: String, required: true },
+        price: { type: Number, required: true },
+        discount: { type: Number },
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'Product',
+          required: true,
         },
+      },
     ],
     shippingAddress: {
-        fullName: { type: String, required: true },
-        address: { type: String, required: true },
-        city: { type: String, required: true },
-        phone: { type: Number, required: true },
+      fullName: { type: String, required: true },
+      address: { type: String, required: true },
+      city: { type: String, required: true },
+      phone: { type: Number, required: true },
     },
     paymentMethod: { type: String, required: true },
     itemsPrice: { type: Number, required: true },
@@ -29,11 +30,12 @@ const orderSchema = new mongoose.Schema({
     isPaid: { type: Boolean, default: false },
     paidAt: { type: Date },
     isDelivered: { type: Boolean, default: false },
+    isConfirmed: { type: Boolean, default: false },
     deliveredAt: { type: Date },
-},
-    {
-        timestamps: true,
-    }
+  },
+  {
+    timestamps: true,
+  },
 );
 const Order = mongoose.model('Order', orderSchema);
-module.exports = Order
+module.exports = Order;
